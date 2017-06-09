b28a0934dd86eed820e90d592794530d4ab77f30
Rob Pataki <rob@handcrafted.studio>
2017-06-09 20:52:48 +0100

    Add some commonly used functionality to the app
    App initialisation in app.js:
    - Device detection
    - FastClick initialisation
    - Init MCP
    - Load and cache global.json file contents
    
    App controller in mcp.js:
    - Initialise UI
    - Reset scroll on page load
    - Handle window resize events

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,6 +1,52 @@
-import RPMenu from 'rp-menu';
+'use strict';
 
-console.log(`app.js has loaded!`)
+// Vendor import
+const BREWSER = require('brewser/dist/brewser.min').br;
 
-// Responsive menu
-const rpMenu = new RPMenu();
+// Module imports
+import MCP from 'modules/mcp';
+
+import json from '../html/data/global.json';
+
+// Cache some globals
+window.BREWSER = BREWSER;
+window.$ = $;
+
+window._app = window._app || {};
+window._app.data = json;
+window._app.DEVICE_MODES = {
+  PHONE: 'phone',
+  TABLET: 'tablet',
+  DESKTOP: 'desktop'
+};
+
+window._app.start = function() {
+  // Set up touch devices
+  if(BREWSER.device.touch) {
+    FastClick.attach(document.body);
+    $('html').addClass('is-touch');
+  }
+
+  // Cache device mode on the app instance
+  var deviceMode = _app.DEVICE_MODES.DESKTOP;
+  if(BREWSER.device.phone) {
+    deviceMode = _app.DEVICE_MODES.PHONE;
+    $('html').addClass('is-phone');
+  } else if(BREWSER.device.tablet) {
+    deviceMode = _app.DEVICE_MODES.TABLET;
+    $('html').addClass('is-tablet');
+  } else {
+    $('html').addClass('is-desktop');
+  }
+  _app.phoneDeviceMode = deviceMode === _app.DEVICE_MODES.PHONE;
+  _app.tabletDeviceMode = deviceMode === _app.DEVICE_MODES.TABLET;
+  _app.desktopDeviceMode = deviceMode === _app.DEVICE_MODES.DESKTOP;
+
+  // Initialise the MCP
+  window._app.mcp = new MCP(window._app);
+}
+
+// On Ready
+$(document).ready(function() {
+  window._app.start();
+});
---
+++ src/javascripts/modules/mcp.js
@@ -0,0 +1,97 @@
+'use strict';
+
+// Vendor imports
+import { TweenMax } from 'gsap';
+const BREWSER = require('brewser/dist/brewser.min').br;
+import RPMenu from 'rp-menu';
+
+// Module imports
+
+export default class MCP {
+  constructor(app) {
+    if (!app) {
+      console.error('[MCP] - `app` is required!');
+      return;
+    }
+
+    this.app = app;
+
+    this._initUI();
+
+    this._resetScrollTop();
+
+    $(window).on(MCP.Events.RESIZE, _.bind(this._handleResize, this));
+    this._handleResize();
+  }
+
+
+
+
+
+////////////////////////////////////
+/////// Private methods
+////////////////
+  _initUI() {
+    // Responsive menu (only an example, will be removed)
+    this.rpMenu = new RPMenu();
+  }
+
+
+  _resetScrollTop() {
+    setTimeout(function(){
+      $(window).scrollTop(0);
+    }, 100);
+  }
+
+  _scrollWindowToPosition(posY = 100, duration = 2.4) {
+    var obj = {
+      y: window.pageYOffset
+    };
+
+    TweenMax.to(obj, duration, {
+      y: posY,
+      ease: Strong.easeInOut,
+      onUpdate: _.bind(function(){
+        $(window).scrollTop(obj.y);
+      }, this)
+    });
+  }
+
+
+
+
+
+////////////////////////////////////
+/////// Event handlers
+////////////////
+
+  _handleResize(e) {
+    $(this.eventDispatcher).trigger(MCP.Events.RESIZE);
+
+    if(this._resizeTimeout) {
+      clearTimeout(this._resizeTimeout);        
+    }
+
+    this._resizeTimeout = setTimeout(_.bind(this._handleDelayedResize, this), MCP.RESIZE_DELAY);
+  }
+
+  _handleDelayedResize(e) {
+    $(this.eventDispatcher).trigger(MCP.Events.DELAYED_RESIZE);
+  }
+}
+
+
+
+
+
+////////////////////////////////////
+/////// Static class vars
+////////////////
+
+// Events
+MCP.Events = {
+  RESIZE: 'resize',
+  DELAYED_RESIZE: 'debouncedResize',
+};
+
+MCP.RESIZE_DELAY = 500;
